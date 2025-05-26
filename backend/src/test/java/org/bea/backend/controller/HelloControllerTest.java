a7de92f5389e4acf308ded71c98aa0a15a1e68a4
learnjavabh <learnmsbh@outlook.de>
2025-05-26 12:58:04 +0200

    added missing test, so sonar will be finally happy

---
+++ backend/src/test/java/org/bea/backend/controller/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package org.bea.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void hello_shouldReturn_HelloWorld() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello, World!"));
+    }
+}
