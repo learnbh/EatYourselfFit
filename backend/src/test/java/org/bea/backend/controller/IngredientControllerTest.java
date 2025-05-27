4806b8599a1890f9b75b806dd75b2b69803433da
learnjavabh <learnmsbh@outlook.de>
2025-05-27 18:38:17 +0200

    added case shouldReturn_List for getIngredients

--- backend/src/test/java/org/bea/backend/controller/IngredientControllerTest.java
+++ backend/src/test/java/org/bea/backend/controller/IngredientControllerTest.java
@@ -1,5 +1,8 @@
 package org.bea.backend.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.bea.backend.model.Ingredient;
+import org.bea.backend.repository.IngredientRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,6 +12,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Collections;
+import java.util.List;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -16,11 +22,26 @@ public class IngredientControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private IngredientRepository mockIngredientRepository;
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    Ingredient milk = new Ingredient("milk", "milk", "low fat", 100.0, "ml", 1.29, "egal");
 
     @Test
     public void getIngredients_shouldReturn_EmptyList_whenDbIsEmpty() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/eyf/ingredients"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+    @Test
+    public void getIngredients_shouldReturn_IngredientsList_whenDbIsNotEmpty() throws Exception {
+        // Given
+        mockIngredientRepository.save(milk);
+        List<Ingredient> expected = Collections.singletonList(milk);
+        mockMvc.perform(MockMvcRequestBuilders.get("/eyf/ingredients"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(mapper.writeValueAsString(expected)));
     }
 }
--- backend/src/test/java/org/bea/backend/service/IngredientServiceTest.java
+++ backend/src/test/java/org/bea/backend/service/IngredientServiceTest.java
@@ -1,26 +1,46 @@
 package org.bea.backend.service;
 
+import org.bea.backend.model.Ingredient;
 import org.bea.backend.repository.IngredientRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class IngredientServiceTest {
 
+    IngredientRepository mockIngredientRepository;
     private IngredientService ingredientService;
+
+    Ingredient milk;
 
     @BeforeEach
     void setUp() {
-        IngredientRepository mockIngredientRepository = Mockito.mock(IngredientRepository.class);
+        mockIngredientRepository = Mockito.mock(IngredientRepository.class);
         ingredientService = new IngredientService(mockIngredientRepository);
+
+        milk = new Ingredient("milk", "milk", "low fat", 100.0, "ml", 1.29, "egal");
     }
 
     @Test
     public void getIngredients_shouldReturn_EmptyList_whenMongoCollectionIsEmpty(){
         assertEquals(Collections.emptyList(), ingredientService.getIngredients());
     }
+
+    @Test
+    public void getIngredients_shouldReturn_IngredientsList_whenAtLeastOneIngredientIsPresent() {
+        // given
+        mockIngredientRepository.save(milk);
+        List<Ingredient> expected = Collections.singletonList(milk);
+        // when
+        Mockito.when(mockIngredientRepository.findAll()).thenReturn(expected);
+        // then
+        assertEquals(expected, ingredientService.getIngredients());
+        Mockito.verify(mockIngredientRepository, Mockito.times(1)).findAll();
+    }
+
 }
