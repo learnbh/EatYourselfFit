a6dbd554852256d0d0e280a9841cea67f206694f
learnjavabh <learnmsbh@outlook.de>
2025-05-27 18:35:16 +0200

    added removed annotation etc. , will do this when I implement the create-method

--- backend/src/main/java/org/bea/backend/model/Ingredient.java
+++ backend/src/main/java/org/bea/backend/model/Ingredient.java
@@ -1,27 +1,14 @@
 package org.bea.backend.model;
 
-import org.jetbrains.annotations.NotNull;
-import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Ingredients")
-@CompoundIndex(def = "{'product' : 1, 'variation' : 1}", unique = true)
+@Document("Ingredients")
 public record Ingredient(
     String id,
-    @NotNull String product,
+    String product,
     String variation,
-    @NotNull Double quantity,
-    @NotNull String unit,
-    double prices,
+    Double quantity,
+    String unit,
+    Double prices,
     String nutrientsId
-) {
-    public Ingredient(String id, String product, String variation, Double quantity, String unit, double prices, String nutrientsId) {
-        this.id = id;
-        this.product = product;
-        this.variation = (variation == null) ? "" : variation;
-        this.quantity = quantity;
-        this.unit = unit;
-        this.prices = prices;
-        this.nutrientsId = nutrientsId;
-    }
-}
+) {}
