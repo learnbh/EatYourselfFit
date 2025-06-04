62f11fd155e848e27e5f5c937218be41c7045d40
learnjavabh <learnmsbh@outlook.de>
2025-06-04 14:05:43 +0200

    added getIngredientByProductAndVariation

--- backend/src/main/java/org/bea/backend/repository/IngredientRepository.java
+++ backend/src/main/java/org/bea/backend/repository/IngredientRepository.java
@@ -4,6 +4,9 @@ import org.bea.backend.model.Ingredient;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IngredientRepository extends MongoRepository<Ingredient, String> {
+    Optional<Ingredient> getIngredientByProductAndVariation(String product, String variation);
 }
