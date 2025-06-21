5f1464bac787d24fc1a8aaab8efcf6a0f6384eb4
learnjavabh <learnmsbh@outlook.de>
2025-06-21 14:22:00 +0200

    changed color

--- frontend/src/component/ShowError.tsx
+++ frontend/src/component/ShowError.tsx
@@ -7,7 +7,7 @@ export default function ShowError(props: Readonly<Props>){
 
     return(
         <>
-            <span className="text-red-800">{ props.message }</span>
+            <span className="text-red-700">{ props.message }</span>
         </>
     )
 }
