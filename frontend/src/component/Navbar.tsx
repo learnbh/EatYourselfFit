7733f92a430007acbd5e4a23e411d3f9f019fb4d
learnjavabh <learnmsbh@outlook.de>
2025-06-26 00:32:15 +0200

    Changed static navbar to hamburger menu; updated navigation icons to better reflect page content

--- frontend/src/component/Navbar.tsx
+++ frontend/src/component/Navbar.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import {useRecipeCart} from "../context/CardRecipeContext.tsx";
 import {Link} from "react-router-dom";
+import {GiCook, GiHotMeal} from "react-icons/gi";
+import {GrSchedule, GrScheduleNew} from "react-icons/gr";
 
 export default function Navbar(){
     const { recipeItems } = useRecipeCart();
@@ -8,7 +10,7 @@ export default function Navbar(){
 
     return (
         <nav className="navbar">
-            <button className="hamburger" onClick={() => setOpen(!open)}>
+            <button className="hamburger pr-2 pl-2" onClick={() => setOpen(!open)}>
                 ☰
             </button>
             <ul className={`nav-links ${open ? 'open' : ''}`}>
@@ -16,6 +18,12 @@ export default function Navbar(){
                 <li><Link to="/recipe">Rezepte</Link></li>
                 <li><Link to="/recipeplan">Rezepte erstellen ({recipeItems.length})</Link></li>
             </ul>
+            <div className={`nav-links ${!open ? 'open' : ''}`}>
+                <Link className="h-5" to="/recipe"><GiCook style = {{ width: "inherit", height:"inherit"}} /></Link>
+                <Link className="h-5" to="/recipeplan"><GiHotMeal style = {{ width: "inherit", height:"inherit"}} /></Link>
+                <Link className="h-5" to="/weekplan"><GrSchedule style = {{ width: "inherit", height:"inherit"}} /></Link>
+                <Link className="h-6" to="/shoppinglist"><GrScheduleNew style = {{ width: "inherit", height:"inherit"}} /></Link>
+            </div>
         </nav>
     );
 }
