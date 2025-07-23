fb38bcc16c61d6ba8ed8e5764cf49b8c71fd60aa
learnjavabh <learnmsbh@outlook.de>
2025-07-23 17:43:53 +0200

    refactoring: Replace all tests with a single Parameterized one.

--- backend/src/test/java/org/bea/backend/utils/StringUtilTest.java
+++ backend/src/test/java/org/bea/backend/utils/StringUtilTest.java
@@ -1,63 +1,25 @@
 package org.bea.backend.utils;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilTest {
 
     StringUtil stringUtil = new StringUtil();
-
-    @Test
-    void mapLowercaseUmlautAndSharpS_shouldReturnMapped_Umlaut_a_To_ae() {
-        // given
-        String name = "tätätä";
-        String nameWithoutUmlaute = "taetaetae";
+    @ParameterizedTest
+    @CsvSource({
+            "Ä, ae",
+            "Fuß, fuss",
+            "tütütü, tuetuetue",
+            "tötötö, toetoetoe",
+            "tätatä, taetatae",
+    })
+    void mapLowercaseUmlautAndSharpS_shouldCorrectMapping_andReturnTrue(String actual, String expected) {
         //when
-        String result = stringUtil.mapLowercaseUmlautAndSharpS(name);
+        String result = stringUtil.mapLowercaseUmlautAndSharpS(actual);
         // then
-        assertTrue(result.contains(nameWithoutUmlaute));
-    }
-
-    @Test
-    void mapLowercaseUmlautAndSharpS_shouldReturnMapped_Umlaut_o_To_oe() {
-        // given
-        String name = "tötötö";
-        String nameWithoutUmlaute = "toetoetoe";
-        //when
-        String result = stringUtil.mapLowercaseUmlautAndSharpS(name);
-        // then
-        assertTrue(result.contains(nameWithoutUmlaute));
-    }
-
-    @Test
-    void mapLowercaseUmlautAndSharpS_shouldReturnMapped_Umlaut_u_To_ue() {
-        // given
-        String name = "tütütü";
-        String nameWithoutUmlaute = "tuetuetue";
-        //when
-        String result = stringUtil.mapLowercaseUmlautAndSharpS(name);
-        // then
-        assertTrue(result.contains(nameWithoutUmlaute));
-    }
-    @Test
-    void mapLowercaseUmlautAndSharpS_shouldReturnMapped_sharp_s_To_ss() {
-        // given
-        String name = "Fuß";
-        String nameWithoutUmlaute = "fuss";
-        //when
-        String result = stringUtil.mapLowercaseUmlautAndSharpS(name);
-        // then
-        assertTrue(result.contains(nameWithoutUmlaute));
-    }
-    @Test
-    void mapLowercaseUmlautAndSharpS_shouldReturnMapped_Umlaut_A_To_ae_InLowerCase() {
-        // given
-        String name = "Ä";
-        String nameWithoutUmlaute = "ae";
-        //when
-        String result = stringUtil.mapLowercaseUmlautAndSharpS(name);
-        // then
-        assertTrue(result.contains(nameWithoutUmlaute));
+        assertTrue(result.contains(expected));
     }
 }
