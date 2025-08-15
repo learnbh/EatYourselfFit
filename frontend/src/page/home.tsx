8565c9f00381c2284ca90f885e17b7a390da25a5
learnjavabh <learnmsbh@outlook.de>
2025-08-16 00:30:29 +0200

    refactoring second step: components IngredientSearch and IngredientNotFound containing now all functionality, it's all at one place

--- frontend/src/page/home.tsx
+++ frontend/src/page/home.tsx
@@ -8,85 +8,16 @@ import { GiCook } from "react-icons/gi";
 import { GiHotMeal } from "react-icons/gi";
 import { GrScheduleNew } from "react-icons/gr";
 import { GrSchedule } from "react-icons/gr";
-import {useCallback, useState} from "react";
+import {useState} from "react";
 import type {Ingredient} from "../types.ts";
-import axios from "axios";
-import IngredientNotFound from "../component/IngredientNotFound.tsx";
-import {useNavigate} from "react-router-dom";
 import IngredientSearchResultItem from "../component/IngredientSearchResultItem.tsx";
+import IngredientNotFound from "../component/IngredientNotFound.tsx";
 
 export default function Home(){
     const [ingredientSearch, setIngredientSearch] = useState<string>("");
     const [ingredientsSearch, setIngredientsSearch] = useState<Ingredient[]>([]);
-    const [ingredientNotFoundVisible, setIngredientNotFoundVisible] = useState(false);
+    const [ingredientNotFoundVisible, setIngredientNotFoundVisible] = useState<boolean>(false);
     const [isLoading, setIsLoading] = useState<boolean>(false);
-
-    const routeTo =useNavigate()
-
-    function abortAddIngredient (){
-        setIngredientSearch("");
-        setIngredientsSearch([]);
-    }
-
-    const editIngredientGeneratedByOpenAi = useCallback(async (search:string) => {
-        if(search.trim() === ""){
-            setIngredientsSearch([]);
-            setIsLoading(false);
-            setIngredientNotFoundVisible(false);
-            return;
-        }
-        try {
-            setIsLoading(true);
-            const response = await axios.get("/eyf/ingredients/name/" + search);
-            if (response.data.length > 0) {
-                const ingredientId:string = response.data[0].id;
-                setIngredientsSearch(response.data);
-                sessionStorage.setItem('detailId',ingredientId);
-                window.open("/ingredient", "_blank");
-                setIngredientNotFoundVisible(false);
-            } else {
-                setIngredientsSearch([]);
-                setIngredientNotFoundVisible(true);
-            }
-        } catch (error){
-            if (axios.isAxiosError(error)) {
-                if (error.response?.data.message) {
-                    console.error('Axios errors:', error.response?.data || error.message);
-                } else {
-                    console.error('Unexpected Axios error:', error);
-                }
-            } else {
-                console.log(error);
-            }
-        }finally {
-            setIsLoading(false);
-        }
-    }, []);
-
-    const addPerOpenAiIngredient = async () => {
-        try {
-            setIngredientNotFoundVisible(false);
-            setIsLoading(true);
-            await axios.post("/eyf/ingredients/openai/add", {product: ingredientSearch, variation:""})
-            await editIngredientGeneratedByOpenAi(ingredientSearch);
-        } catch (error){
-            if (axios.isAxiosError(error)) {
-                if (error.response?.data.message) {
-                    console.error('Axios errors:', error.response?.data || error.message);
-                } else {
-                    console.log("error: "+error);
-                    alert('Es konnten keine Nährstoffe gefunden werden. Änderne die Anfrage und versuche es erneut.');
-                }
-            } else {
-                console.log(error);
-            }
-        }finally {
-            setIsLoading(false);
-        }
-    }
-    function addPerUser(){
-        routeTo("/ingredient/add/"+ingredientSearch);
-    }
 
     return(
         <>
@@ -95,27 +26,31 @@ export default function Home(){
                     <IngredientSearch
                         placeholder="Zutat suchen"
                         name="addingredient"
+                        searchWord={ingredientSearch}
+                        setSearchWord={setIngredientSearch}
                         setSearchResult={setIngredientsSearch}
                         setSearchNotFoundVisible={setIngredientNotFoundVisible}
                         setIsLoading={setIsLoading}
                     />
-                </div>
-                <div className="space-y-4">
-                    {ingredientNotFoundVisible && (
-                        <IngredientNotFound
-                            addPerAi={addPerOpenAiIngredient}
-                            addPerUser={addPerUser}
-                            abort={abortAddIngredient}
-                        />
-                    )}
                 </div>
                 <div className={ingredientsSearch.length > 0 ? "flex flex-wrap gap-4 border sm:pb-2 sm:pt-2 justify-center":""}>
                     {ingredientsSearch.length > 0 && (
                         ingredientsSearch.map((i:Ingredient) => <IngredientSearchResultItem
                                 key = {i.id}
                                 ingredient={i}
-                            ></IngredientSearchResultItem>
+                            />
                         )
+                    )}
+                </div>
+                <div className="space-y-4">
+                    {ingredientNotFoundVisible && (
+                        <IngredientNotFound
+                            searchWord={ingredientSearch}
+                            setSearchWord={setIngredientSearch}
+                            setSearchResult={setIngredientsSearch}
+                            setSearchNotFoundVisible={setIngredientNotFoundVisible}
+                            setIsLoading={setIsLoading}
+                        />
                     )}
                 </div>
                     {isLoading && (
