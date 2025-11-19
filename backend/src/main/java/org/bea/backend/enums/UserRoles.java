2cc81fe13726cf78c8e9796a1b04c4f7cc35ad6b
learnjavabh <learnmsbh@outlook.de>
2025-11-19 12:49:09 +0100

    refactoring unused function

--- backend/src/main/java/org/bea/backend/enums/UserRoles.java
+++ backend/src/main/java/org/bea/backend/enums/UserRoles.java
@@ -3,13 +3,4 @@ package org.bea.backend.enums;
 public enum UserRoles {
     USER,
     ADMIN;
-
-    public static boolean isValidRole(String role) {
-        for (UserRoles userRole : UserRoles.values()) {
-            if (userRole.name().equalsIgnoreCase(role)) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
