8f66f0d5c9a40f4764f2db13a9fcdd31ccbf6977
learnjavabh <learnmsbh@outlook.de>
2025-11-19 12:30:50 +0100

    added a collation name

--- backend/src/main/java/org/bea/backend/repository/UserReprository.java
+++ backend/src/main/java/org/bea/backend/repository/UserReprository.java
@@ -1,9 +1,11 @@
 package org.bea.backend.repository;
 
 import org.bea.backend.model.User;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Document(collation = "Users")
 public interface UserReprository extends MongoRepository<User, String>{
 }
